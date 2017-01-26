f069da3081dd58c869892052ab4edc4d15524a56
Kelvin <manchunw@gmail.com>
2017-01-26 12:28:46 +0800

    Variables and enums

--- Program.cs
+++ Program.cs
@@ -4,8 +4,18 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        public enum CarType {
+            Toyota = 1,
+            Honda = 2,
+            Ford = 3
+        }
         public static void Main(string[] args)
         {
+            string productName = "TV";
+            int productYear = 2012;
+            double productPrice = 279.99;
+            CarType myCarType = CarType.Toyota;
+            Console.WriteLine(productName + " in " + productYear + ": " + productPrice);
             Console.WriteLine("Hello World!");
         }
     }
